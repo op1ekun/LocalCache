c79c35b60afeebe1118ad064e847e784f536de90
op1ekun <woociek@gmail.com>
2012-07-17 20:44:40 +0200

    First part of code, testrunner missing

---
+++ t/Cache.test.js
@@ -0,0 +1,124 @@
+$(document).ready(function(){
+    module("Cache", {
+        setup : function() {
+            this.storage    = sempro.core.Storage;
+            this.cache      = sempro.core.Cache;
+            
+            this.makeData   = function(end) {
+                var data = "some";
+                
+                for (var i = 0; i < end; i++) {
+                    var temp = data + data + "somethingElse";
+                    data = temp;
+                }
+                
+                return data;                
+            };
+            
+            // fake Date
+            this.origDate   = Date;
+            Date = function() {
+                this.getTime    = function() { return 100; }
+            }
+
+            this.storage.reset();            
+        },
+        
+        teardown : function() {
+            this.cache.setTTL(86400000);
+            this.storage.reset();
+            
+            // reset to original Date
+            Date = this.origDate;
+        }
+    });
+    
+    test("getTTL method", function(){
+        expect(1);
+        
+        equal(this.cache.getTTL(), 86400000, "get TTL value");
+    });
+    
+    test("setTTL method", function() {
+        expect(1);
+        
+        this.cache.setTTL(12345);
+        
+        equal(this.cache.getTTL(), 12345, "set TTL value");         
+    });
+    
+    test("resetTTL method", function() {
+        expect(1);
+        
+        this.cache.resetTTL();
+        
+        equal(this.cache.getTTL(), 86400000, "reset TTL value");
+    });
+    
+    test("cache method", function(){
+        expect(2);
+        
+        var testJSON = {
+            "outer" : {
+                "inner" : "someValue"
+            }            
+        };
+
+        // "initialize" ttlList in storage        
+        this.storage.add("ttlList", {});
+        ok(this.cache.cache("jsonKey", testJSON), "JSON succesfully cached");
+        
+        // about 280KB of data
+        var data = this.makeData(14);
+        
+        var that = this;
+        function fillCache() {
+            for (var i = 0, l = 17; i < l; i++) {
+                Date = function() {
+                    this.getTime    = function() { return 100 + i; }
+                    
+                    if (i == 7) {
+                        this.getTime    = function() { return 120; }                        
+                    }
+                }
+                
+                that.cache.cache("data" + i, data);
+            }
+        }
+        
+        fillCache();
+        
+        // by this moment the Storage is almost full
+        // adding another item will raise an exception in Storage.add()
+        // Cache should gracefully remove the oldest items 
+        // to free storage space for the new item
+        ok(this.cache.cache("data", data+data), "cache was gracefully cleared - new item cached");
+    });
+
+    test("invalidate method", function(){
+        expect(2);
+        
+        // stuff older than 300ms should be removed from the cache
+        this.cache.setTTL(300);
+        
+        // "initialize" ttlList in storage        
+        this.storage.add("ttlList", {});
+        
+        // first cache        
+        this.cache.cache("stringKey", "someString");
+        
+        // to make item older than it appears ;)
+        Date = function() {
+            this.getTime    = function() { return 500; }
+        }
+        
+        this.cache.invalidateOld();
+        
+        var stringItem  = this.storage.get("stringKey");
+        equal(stringItem, undefined, "remove item");
+                
+        var ttlList     = this.storage.get("ttlList");
+        equal(ttlList["stringKey"], undefined, "remove timestamp for item");
+    });
+    
+});
---
+++ t/Storage.test.js
@@ -0,0 +1,147 @@
+$(document).ready(function(){
+    module("Storage", {
+        setup : function() {
+            this.storage    = sempro.core.Storage;
+
+            this.makeData   = function(end) {
+                var data = "some";
+                
+                for (var i = 0; i < end; i++) {
+                    var temp = data + data + "somethingElse";
+                    data = temp;
+                }
+                
+                return data;                
+            };
+            
+            this.storage.reset();
+        },
+        
+        teardown : function() {
+            this.storage.reset();
+        }
+    });
+    
+    test("add method", function() {
+        expect(5);
+        
+        this.storage.add("jsonKey", {
+            "testStorage" : {
+                "test" : 1
+            }
+        });
+        // test if method DID NOT raise an exception
+        ok(true, "store JSON");
+        
+        this.storage.add("stringKey", "someString");
+        ok(true, "store String");
+        
+        this.storage.add("numberKey", 3);
+        ok(true, "store Number");
+        
+        this.storage.add("nullKey", null);
+        ok(true, "store null");
+        
+        // try to overwrite existing item
+        raises(function() { this.storage.add("stringKey", "anotherString", true); }, function (e) { return e.name === "Error"; }, "doNotOverwrite flag is on - cannot overwrite existing item"); 
+    });
+    
+    test("get method", function() {
+        expect(4);
+        
+        this.storage.add("jsonKey", {
+            "testStorage" : {
+                "test" : 1
+            }
+        });
+        
+        var jsonItem    = this.storage.get("jsonKey");
+        deepEqual(jsonItem, {
+            "testStorage" : {
+                "test" : 1
+            }
+        }, "retrieve JSON");
+        
+                            this.storage.add("stringKey", "someString");
+        var stringItem  =   this.storage.get("stringKey");
+        equal(stringItem, "someString", "retrieve String");
+        
+                            this.storage.add("numberKey", 3);
+        var numberItem  =   this.storage.get("numberKey");
+        equal(numberItem, 3, "retrieve Number");
+        
+                            this.storage.add("nullKey", null);
+        var nullItem    =   this.storage.get("nullKey");
+        equal(nullItem, null, "retrieve null");         
+    });
+    
+    test("remove method", function() {
+        expect(2);
+        
+        this.storage.add("jsonKey", {
+            "testStorage" : {
+                "test" : 1
+            }
+        });
+        equal(this.storage.remove("jsonKey"), true, "remove item");
+        
+        equal(this.storage.remove("fakeKey"), false, "remove non existing item");
+    });
+    
+    test("reset method", function() {
+        expect(1);
+        
+        this.storage.add("stringKey", "someString");
+        
+        this.storage.reset();
+        
+        equal(this.storage.get("stringKey"), undefined, "reset storage");         
+    });
+    
+    test("getAvailableSpace method", function() {
+        expect(4);
+        
+        var expectedSize    = 5 * 1024 * 1024; // bytes
+        
+        expectedSize        = expectedSize - JSON.stringify(expectedSize).length;
+        
+        equal(this.storage.getAvailableSpace(), expectedSize, "storage is empty");
+        
+        var jsonItem = {
+            "some"  : "json",
+            "wiht"  : "random data 123"            
+        };
+        var expectedUse     = JSON.stringify(jsonItem).length;
+        // storage will have one item stored
+        expectedSize        = expectedSize - expectedUse;
+        
+        this.storage.add("jsonKey", jsonItem);
+        equal(this.storage.getAvailableSpace(), expectedSize, "storage has one item");
+
+
+        var stringItem      = "someString";
+        
+        expectedUse         = JSON.stringify(stringItem).length;
+        // storage will have two items stored
+        expectedSize        = expectedSize - expectedUse;
+        
+        this.storage.add("stringKey", stringItem);
+        equal(this.storage.getAvailableSpace(), expectedSize, "storage has two items");
+
+        
+        expectedUse         = JSON.stringify(jsonItem).length;
+        // storage will have one item stored
+        expectedSize        = expectedSize + expectedUse;
+        
+        this.storage.remove("jsonKey");
+        equal(this.storage.getAvailableSpace(), expectedSize, "item removed, storage has one item");
+    });
+    
+    test("exceed Storage space limit", function() {
+        expect(1);
+        
+        var data = this.makeData(20);
+        
+        raises(function() { this.storage.add("dataKey", data)}, function(e) { return e.name === "RangeError"; }, "size of item exceeds capacity of storage, RangeError thrown");
+    });
+});
